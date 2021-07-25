354189e65d51b51bf1905866b73c134fc94234c4
Sergio Forcen <sergiocalipso@gmail.com>
2021-07-25 15:56:58 +0200

    Add LearningPath and Course models in Courses

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "hello",
     "users",
+    "courses",
 ]
 
 MIDDLEWARE = [
@@ -117,4 +118,16 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
 
+# Media files
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
+
+# Login url
+LOGIN_URL = 'login' 
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 django_heroku.settings(locals())
--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -6,6 +6,12 @@ admin.autodiscover()
 
 import hello.views
 
+"""
+Librerias para poder visualizar imagenes o media desde el panel de administracion.
+"""
+from django.conf import settings
+from django.conf.urls.static import static
+
 # To add a new path, first import the app:
 # import blog
 #
@@ -18,4 +24,7 @@ urlpatterns = [
     path("", hello.views.index, name="index"),
     path("db/", hello.views.db, name="db"),
     path("admin/", admin.site.urls),
-]
+
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT) # Configurado en settings.py para mostrar media durante desarrollo
+
+
