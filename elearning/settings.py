60895318ee6da7f0a1b4e00e219caa8b08fc6a13
Sergio Forcen <sergiocalipso@gmail.com>
2021-08-01 14:24:29 +0200

    Fixing stuf

--- elearning/settings.py
+++ elearning/settings.py
@@ -122,11 +122,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR)
+STATIC_ROOT = os.path.join(BASE_DIR,'/static')
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [
-    os.path.join(STATIC_ROOT,'/staticfiles')
+    os.path.join(BASE_DIR,'/static'),
+    os.path.join(BASE_DIR,'/staticfiles'),
 ]
+
+
 # Media files
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
